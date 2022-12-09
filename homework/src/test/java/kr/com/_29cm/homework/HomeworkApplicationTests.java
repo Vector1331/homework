33964dc51ac0cc9f7cef45006d8d998234a067db
Vector1331 <tpgus4796@gmail.com>
2022-12-09 16:08:19 +0900

    create project

---
+++ homework/src/test/java/kr/com/_29cm/homework/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.com._29cm.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
